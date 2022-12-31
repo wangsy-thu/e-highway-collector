6d3b1415c6be466781ac389432ea342828b85230
WangSongyao <1178663139@qq.com>
2022-12-31 13:28:12 +0800

    修改细节

--- flux/line.go
+++ flux/line.go
@@ -3,6 +3,6 @@ package flux
 type Line struct {
 	Measurement string                 `json:"measurement"`
 	Tags        map[string]string      `json:"tags"`
-	Fields      map[string]interface{} `json:"Fields"`
+	Fields      map[string]interface{} `json:"fields"`
 	Timestamp   int                    `json:"timestamp"`
 }
